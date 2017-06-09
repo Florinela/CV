d4c7f8c211297374a4eabe634c25d111ccfc4635
Florinela <zuudfer@gmail.com>
2017-06-09 22:55:55 +0300

    CV
    CV project

---
+++ cv/assets/js/main.js
@@ -0,0 +1,80 @@
+var a = function(){
+       $("#test-circle").circliful({
+            animation: 1,
+            animationStep: 5,
+            foregroundBorderWidth: 10,
+            backgroundBorderWidth: 10,
+            percent: 40,
+            textSize: 28,
+            textStyle: 'font-size: 12px;',
+            textColor: '#666',
+            multiPercentage: 1,
+            percentages: [10, 20, 30]
+        });
+}
+
+a();
+var b = function(){
+       $("#test-circle2").circliful({
+            animation: 1,
+            animationStep: 5,
+            foregroundBorderWidth: 10,
+            backgroundBorderWidth: 10,
+            percent: 60,
+            textSize: 28,
+            textStyle: 'font-size: 12px;',
+            textColor: '#666',
+            multiPercentage: 1,
+            percentages: [10, 20, 30]
+        });
+}
+
+b();
+
+var c = function(){
+       $("#test-circle3").circliful({
+            animation: 1,
+            animationStep: 5,
+            foregroundBorderWidth: 10,
+            backgroundBorderWidth: 10,
+            percent: 50,
+            textSize: 28,
+            textStyle: 'font-size: 12px;',
+            textColor: '#666',
+            multiPercentage: 1,
+            percentages: [10, 20, 30]
+        });
+}
+
+c();
+var d = function(){
+       $("#test-circle4").circliful({
+            animation: 1,
+            animationStep: 5,
+            foregroundBorderWidth: 10,
+            backgroundBorderWidth: 10,
+            percent: 70,
+            textSize: 28,
+            textStyle: 'font-size: 12px;',
+            textColor: '#666',
+            multiPercentage: 1,
+            percentages: [10, 20, 30]
+        });
+}
+
+d();
+var e=function(){
+      $("test-circle5").circliful({
+            animation:1,
+            animationStep:5,
+            foregroundBorderWidth: 10,
+            backgroundBorderWidth: 10,
+            percent: 60,
+            tcxtSize: 28,
+            textStyle: 'font-size: 12px;',
+            textColor: '#666',
+            multiPercentage: 1,
+            percentages: [10,20,30]
+      });
+}
+e();
